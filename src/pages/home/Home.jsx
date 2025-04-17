f5bd0394bd867f434b5c45c4610b898d7a06e3c8
ali89akbar <alia42268@gmail.com>
2025-04-18 01:54:24 +0500

    Feature section implemented

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Sidebar from '../../components/Sidebar/Sidebar'
 import './home.scss';
 import Navbar from '../../components/navbar/Navbar';
 import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured';
+import Chart from '../../components/chart/Chart';
 const Home = () => {
   return (
     <div className='home'>
@@ -15,6 +17,10 @@ const Home = () => {
   <Widget type="earning"/>
   <Widget type="balance"/>
   
+</div>
+<div className="charts">
+  <Featured/>
+  <Chart/>
 </div>
 </div>
   </div>
